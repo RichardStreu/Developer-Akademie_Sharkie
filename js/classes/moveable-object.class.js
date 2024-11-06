891d97443d1ed2e39cfcb34e676387ce3aa1140e
Richard Streu <rstreu13@icloud.com>
2024-11-06 11:03:24 +0100

    added class MoveableObject

--- js/classes/moveable-object.class.js
+++ js/classes/moveable-object.class.js
@@ -1,25 +1,27 @@
 export class MoveableObject {
-  x;
-  y;
+  x = 100;
+  y = 100;
   img;
+
+  constructor(x, y, img) {
+    if (x) this.x = x;
+    if (y) this.y = y;
+    if (img) this.img = img;
+  }
 
   moveRight() {
     console.log("Moving Right");
-    
   }
 
   moveLeft() {
     console.log("Moving Left");
-    
   }
 
   moveUp() {
     console.log("Moving Up");
-    
   }
 
   moveDown() {
     console.log("Moving Down");
-    
   }
 }
--- js/script.js
+++ js/script.js
@@ -3,23 +3,19 @@ window.MoveableObject = MoveableObject;
 
 let canvas;
 let ctx;
-let characterSharkie = new Image();
-window.characterSharkie = characterSharkie;
+let character = new MoveableObject(200, null, "hallo Welt");
+window.character = character;
 window.ctx = ctx;
 
 function init() {
   canvas = document.getElementById("canvas");
-  characterSharkie.src = "../assets/img/1.Sharkie/1.IDLE/1.png";
-
   const dpr = window.devicePixelRatio || 1;
   canvas.width = canvas.clientWidth * dpr;
   canvas.height = canvas.clientHeight * dpr;
-
   ctx = canvas.getContext("2d");
   ctx.scale(dpr, dpr);
 
-  setTimeout(() => {
-    ctx.drawImage(characterSharkie, 20, 20, 450, 350);
-  }, 500);
+  console.log(character);
+  
 }
 window.init = init;
