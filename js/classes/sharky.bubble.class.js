b70f40926479d4df48f2d31393cc386756b5a0c9
Richard Streu <rstreu13@icloud.com>
2024-11-18 07:34:08 +0100

    bubbles are moving in rigth direction

--- js/classes/sharky.bubble.class.js
+++ js/classes/sharky.bubble.class.js
@@ -1,11 +1,18 @@
+import { canvasHeight, canvasWidth } from "../script.js";
+
 import { MoveableObject } from "./moveable-object.class.js";
 
 import { sharkyXPosition, sharkyYPosition, sharkyWidth, sharkyHeight } from "./sharky.class.js";
 
 export class SharkyBubble extends MoveableObject {
 
+  direction;
+  bubbleMoveInterval;
+  hasBubbleHit = false;
+
   constructor(direction) {
     super();
+    this.direction = direction;
     if (!this.isEnoughPoison) this.loadImage("../../assets/img/1.Sharkie/4.Attack/Bubble trap/Bubble.png");
     if (this.isEnoughPoison) this.loadImage("../../assets/img/1.Sharkie/4.Attack/Bubble trap/Poisoned Bubble (for whale).png");
     if (direction == "left") {
@@ -17,7 +24,13 @@ export class SharkyBubble extends MoveableObject {
     }
     this.width = 25;
     this.height = 25;
+    this.moveBubble();
   }
 
-  updateBubblePosition() {}
+  moveBubble() {
+    this.bubbleMoveInterval = setInterval(() => {
+      if (this.direction == "left" && this.x > -this.width && !this.hasBubbleHit) this.x -= 5;
+      if (this.direction == "right" && this.x < canvasWidth * 4 && !this.hasBubbleHit) this.x += 5;
+    }, 10);
+  }
 }
