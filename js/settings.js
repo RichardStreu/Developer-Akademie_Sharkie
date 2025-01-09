0fae1648c1b53d8cd0d93074a58e5f352b5fabf0
Richard Streu <rstreu13@icloud.com>
2025-01-09 11:15:57 +0100

    feat: update control screen navigation and enhance submenu functionality with data attributes

--- js/settings.js
+++ js/settings.js
@@ -1,7 +1,7 @@
-export function showSubMenu(id) { 
-  const navBar = document.getElementById("controlNavBar");
-  Array.from(navBar.querySelectorAll("button")).forEach((button) => {
-    button.classList.remove("btnActive");
-  });
-  document.getElementById(id).classList.add("btnActive");
+export function showSubMenu(groupNumber) {
+  const controlScreen = document.getElementById("controlScreen");
+  Array.from(controlScreen.querySelectorAll("button[data-group]")).forEach((button) => button.classList.remove("btnActive"));
+  Array.from(controlScreen.querySelectorAll("div[data-group]")).forEach((div) => div.classList.add("d_none"));
+  document.querySelector(`button[data-group="${groupNumber}"]`).classList.add("btnActive");
+  document.querySelector(`div[data-group="${groupNumber}"]`).classList.remove("d_none");
 }
