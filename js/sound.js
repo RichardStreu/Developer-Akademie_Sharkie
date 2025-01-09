be2f1101718276a39b5b7c9e7b4f9318a1b45a0c
Richard Streu <rstreu13@icloud.com>
2025-01-09 15:46:59 +0100

    feat: implement sound control functionality and add mute/unmute icons

--- js/sound.js
+++ js/sound.js
@@ -1,6 +1,7 @@
 let isSoundMuted = false;
 
 export function muteUnmuteSound() {
+  let muteImage = document.getElementById("muteImage");
   if (!isSoundMuted) {
     console.log("Sound is muted");
   } else {
