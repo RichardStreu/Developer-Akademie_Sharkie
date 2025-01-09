c8ed3ff25c3660b4de0871e9d8b3a5173328968f
Richard Streu <rstreu13@icloud.com>
2025-01-09 10:25:22 +0100

    feat: add control navigation bar and enhance showSubMenu functionality

--- js/settings.js
+++ js/settings.js
@@ -1,3 +1,7 @@
-export function showSubMenu(id) {
-
+export function showSubMenu(id) { 
+  const navBar = document.getElementById("controlNavBar");
+  Array.from(navBar.querySelectorAll("button")).forEach((button) => {
+    button.classList.remove("btnActive");
+  });
+  document.getElementById(id).classList.add("btnActive");
 }
