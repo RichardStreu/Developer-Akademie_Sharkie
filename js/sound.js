781b289b22a72157d1220f05e110a38737eb97c7
Richard Streu <rstreu13@icloud.com>
2025-01-13 12:08:49 +0100

    feat: enhance sound management by initializing audio objects for hurt, slap, blub, electroShock, and chewbacca sounds

--- js/sound.js
+++ js/sound.js
@@ -1,8 +1,20 @@
 let isSoundMuted = false;
 
+let basicVolume = 1;
+
 let musicVolume = 1;
 
 let sfxVolume = 1;
+
+let currentHurtSound;
+let isCurrentHurtSoundPlaying = false;
+
+let currentSlapSound;
+let isCurrentSlapSoundPlaying = false;
+
+let currentBlubSound;
+let isCurrentBlubSoundPlaying = false;
+
 
 let sounds = {
   backgroundRetroArcade: {
@@ -12,26 +24,31 @@ let sounds = {
   },
   hurt: {
     hurt1: {
+      hurt1: new Audio (this.link),
       link: "./assets/audio/edit/hurt/hurt1.mp3",
       volume: 0.5,
       loop: false,
     },
     hurt2: {
+      hurt2: new Audio (this.link),
       link: "./assets/audio/edit/hurt/hurt2.mp3",
       volume: 0.5,
       loop: false,
     },
     hurt3: {
+      hurt3: new Audio (this.link),
       link: "./assets/audio/edit/hurt/hurt3.mp3",
       volume: 0.5,
       loop: false,
     },
     hurt4: {
+      hurt4: new Audio (this.link),
       link: "./assets/audio/edit/hurt/hurt4.mp3",
       volume: 0.5,
       loop: false,
     },
     hurt5: {
+      hurt5: new Audio (this.link),
       link: "./assets/audio/edit/hurt/hurt5.mp3",
       volume: 0.5,
       loop: false,
@@ -39,42 +56,49 @@ let sounds = {
   },
   slap: {
     slap1: {
+      slap1: new Audio (this.link),
       link: "./assets/audio/edit/slap/slap1.mp3",
       volume: 0.5,
       loop: false,
     },
     slap2: {
+      slap2: new Audio (this.link),
       link: "./assets/audio/edit/slap/slap2.mp3",
       volume: 0.5,
       loop: false,
     },
     slap3: {
+      slap3: new Audio (this.link),
       link: "./assets/audio/edit/slap/slap3.mp3",
       volume: 0.5,
       loop: false,
     },
-    slap4: {
+    slap4: {slap4: new Audio (this.link),
       link: "./assets/audio/edit/slap/slap4.mp3",
       volume: 0.5,
       loop: false,
     },
     slap5: {
+      slap5: new Audio (this.link),
       link: "./assets/audio/edit/slap/slap5.mp3",
       volume: 0.5,
       loop: false,
     },
   },
   blub: {
+    blub: new Audio (this.link),
     link: "./assets/audio/edit/blub.mp3",
     volume: 0.5,
     loop: false,
   },
   electroShock: {
+    electroShock: new Audio (this.link),  
     link: "./assets/audio/edit/electro.mp3",
     volume: 0.5,
     loop: false,
   },
   chewbacca: {
+    chewbacca: new Audio (this.link),
     link: "./assets/audio/chewbacca.swf.mp3",
     volume: 0.5,
     loop: false,
