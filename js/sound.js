63bca5f9761e280ac82da3306663110ca169dd2a
Richard Streu <rstreu13@icloud.com>
2025-01-17 09:17:50 +0100

    feat: update music volume control to initialize with default value and enhance gradient background

--- js/sound.js
+++ js/sound.js
@@ -3,7 +3,7 @@ let firstSoundInit = false;
 let basicVolume = 1;
 let isSoundMuted = false;
 
-let musicVolume = 0.5;
+let musicVolume = 0.8;
 let isMusicMuted = false;
 let currentMusic;
 
@@ -28,150 +28,175 @@ let sounds = {
     link: "./assets/audio/retro-game-arcade-236133.mp3",
     volume: 0.1,
     loop: true,
+    type: "music"
   },
   backgroundMetal: {
     audio: new Audio("./assets/audio/metal.mp3"),
     link: "./assets/audio/metal.mp3",
     volume: 0.5,
     loop: true,
+    type: "music"
   },
   backgroundLose: {
     audio: new Audio("./assets/audio/lose-music.mp3"),
     link: "./assets/audio/lose-music.mp3",
     volume: 0.4,
     loop: true,
+    type: "music"
   },
   backgroundWin: {
     audio: new Audio("./assets/audio/winMusic.mp3"),
     link: "./assets/audio/winMusic.mp3",
     volume: 0.5,
     loop: true,
+    type: "music"
   },
   hurt1: {
     audio: new Audio("./assets/audio/edit/hurt/hurt1.mp3"),
     link: "./assets/audio/edit/hurt/hurt1.mp3",
     volume: 0.5,
     loop: false,
+    type: "sfx"
   },
   hurt2: {
     audio: new Audio("./assets/audio/edit/hurt/hurt2.mp3"),
     link: "./assets/audio/edit/hurt/hurt2.mp3",
     volume: 0.5,
     loop: false,
+    type: "sfx"
   },
   hurt3: {
     audio: new Audio("./assets/audio/edit/hurt/hurt3.mp3"),
     link: "./assets/audio/edit/hurt/hurt3.mp3",
     volume: 0.5,
     loop: false,
+    type: "sfx"
   },
   hurt4: {
     audio: new Audio("./assets/audio/edit/hurt/hurt4.mp3"),
     link: "./assets/audio/edit/hurt/hurt4.mp3",
     volume: 0.5,
     loop: false,
+    type: "sfx"
   },
   hurt5: {
     audio: new Audio("./assets/audio/edit/hurt/hurt5.mp3"),
     link: "./assets/audio/edit/hurt/hurt5.mp3",
     volume: 0.5,
     loop: false,
+    type: "sfx"
   },
   slap1: {
     audio: new Audio("./assets/audio/edit/slap/slap1.mp3"),
     link: "./assets/audio/edit/slap/slap1.mp3",
     volume: 0.5,
     loop: false,
+    type: "sfx"
   },
   slap2: {
     audio: new Audio("./assets/audio/edit/slap/slap2.mp3"),
     link: "./assets/audio/edit/slap/slap2.mp3",
     volume: 0.5,
     loop: false,
+    type: "sfx"
   },
   slap3: {
     audio: new Audio("./assets/audio/edit/slap/slap3.mp3"),
     link: "./assets/audio/edit/slap/slap3.mp3",
     volume: 0.5,
     loop: false,
+    type: "sfx"
   },
   slap4: {
     audio: new Audio("./assets/audio/edit/slap/slap4.mp3"),
     link: "./assets/audio/edit/slap/slap4.mp3",
     volume: 0.5,
     loop: false,
+    type: "sfx"
   },
   slap5: {
     audio: new Audio("./assets/audio/edit/slap/slap5.mp3"),
     link: "./assets/audio/edit/slap/slap5.mp3",
     volume: 0.4,
     loop: false,
+    type: "sfx"
   },
   blub: {
     audio: new Audio("./assets/audio/edit/blub.mp3"),
     link: "./assets/audio/edit/blub.mp3",
     volume: 0.2,
     loop: false,
+    type: "sfx"
   },
   electroShock: {
     audio: new Audio("./assets/audio/edit/electro.mp3"),
     link: "./assets/audio/edit/electro.mp3",
     volume: 0.5,
     loop: false,
+    type: "sfx"
   },
   chewbacca: {
     audio: new Audio("./assets/audio/chewbacca.swf.mp3"),
     link: "./assets/audio/chewbacca.swf.mp3",
     volume: 0.4,
     loop: false,
+    type: "sfx"
   },
   swim: {
     audio: new Audio("./assets/audio/edit/swim-short.mp3"),
     link: "./assets/audio/edit/swim-short.mp3",
     volume: 0.6,
     loop: true,
+    type: "sfx"
   },
   coin: {
     audio: new Audio("./assets/audio/edit/subway-surfers-coin-collect.mp3"),
     link: "./assets/audio/edit/subway-surfers-coin-collect.mp3",
     volume: 1,
     loop: false,
+    type: "sfx"
   },
   bottle: {
     audio: new Audio("./assets/audio/edit/bottle-pop-open.mp3"),
     link: "./assets/audio/edit/bottle-pop-open.mp3",
     volume: 0.5,
     loop: false,
+    type: "sfx"
   },
   hover: {
     audio: new Audio("./assets/audio/edit/hover-sound-effect.mp3"),
     link: "./assets/audio/edit/hover-sound-effect.mp3",
     volume: 0.15,
     loop: false,
+    type: "sfx"
   },
   click: {
     audio: new Audio("./assets/audio/edit/button-click.mp3"),
     link: "./assets/audio/edit/button-click.mp3",
     volume: 0.1,
     loop: false,
+    type: "sfx"
   },
   bossSplash: {
     audio: new Audio("./assets/audio/edit/water-for-drama.mp3"),
     link: "./assets/audio/edit/water-for-drama.mp3",
     volume: 0.4,
     loop: false,
+    type: "sfx"
   },
   bossScream: {
     audio: new Audio("./assets/audio/chewbacca.swf.mp3"),
     link: "./assets/audio/chewbacca.swf.mp3",
     volume: 0.3,
     loop: false,
+    type: "sfx"
   },
   snore: {
     audio: new Audio("./assets/audio/snorker.mp3"),
     link: "./assets/audio/snorker.mp3",
-    volume: 0.2,
+    volume: 0,
     loop: true,
+    type: "sfx"
   },
 };
 
@@ -190,7 +215,7 @@ export function initHoverSound() {
 
 export function playSfxSound(sound, delay = 0, loop = false, currentTime = 0) {
   let soundToPlay = sounds[sound].audio;
-  soundToPlay.volume = sounds[sound].volume * sfxVolume * basicVolume;
+  sounds[sound].type === "music" ? (soundToPlay.volume = sounds[sound].volume * musicVolume * basicVolume) : (soundToPlay.volume = sounds[sound].volume * sfxVolume * basicVolume);
   soundToPlay.loop = loop;
   soundToPlay.currentTime = currentTime;
   setTimeout(() => {
@@ -275,18 +300,19 @@ export function muteUnmuteSound() {
   isSoundMuted = !isSoundMuted;
 }
 
-export function changeMusicVolume(category, volume) {
-  musicVolume = volume / 100;
-  if (category === "music") categoryMusicVolumeChange(volume);
+export function changeMusicVolume(category, value) {
+  let newVolume = value / 100;
+  document.getElementById(`${category == "music" ? "musicVolume" : "masterVolume"}`).style.background = `linear-gradient(to right, rgb(127, 255, 224) ${value}%, rgb(58, 124, 108) ${value}%)`;
+
+  if (category === "music") categoryMusicVolumeChange(newVolume);
 }
 window.changeMusicVolume = changeMusicVolume;
 
-function categoryMusicVolumeChange(volume) {
-  document.getElementById("musicVolume").style.background = `linear-gradient(to right, rgb(127, 255, 224) ${volume}%, rgb(58, 124, 108) ${volume}%)`;
-  sounds.backgroundRetroArcade.audio.volume = sounds.backgroundRetroArcade.volume * basicVolume * musicVolume;
-  sounds.backgroundMetal.audio.volume = sounds.backgroundMetal.volume * basicVolume * musicVolume;
-  sounds.backgroundLose.audio.volume = sounds.backgroundLose.volume * basicVolume * musicVolume;
-  sounds.backgroundWin.audio.volume = sounds.backgroundWin.volume * basicVolume * musicVolume;
+function categoryMusicVolumeChange(newVolume) {
+  sounds.backgroundRetroArcade.audio.volume = sounds.backgroundRetroArcade.volume * basicVolume * newVolume;
+  sounds.backgroundMetal.audio.volume = sounds.backgroundMetal.volume * basicVolume * newVolume;
+  sounds.backgroundLose.audio.volume = sounds.backgroundLose.volume * basicVolume * newVolume;
+  sounds.backgroundWin.audio.volume = sounds.backgroundWin.volume * basicVolume * newVolume;
 }
 
 function categoryMasterVolumeChange() {}
